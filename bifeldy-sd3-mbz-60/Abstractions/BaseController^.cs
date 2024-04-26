14adcc086793bf4525e38c718876c9f009f576c8
B. Bias A. Ch <bias@indomaret.co.id>
2024-04-26 09:43:30 +0700

    Murni OOP ~

--- bifeldy-sd3-mbz-60/Abstractions/BaseController^.cs
+++ bifeldy-sd3-mbz-60/Abstractions/BaseController^.cs
@@ -1,12 +1,15 @@
 ﻿using System.Data;
+using System.Text;
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 
+using bifeldy_sd3_lib_60.Abstractions;
 using bifeldy_sd3_lib_60.Databases;
 using bifeldy_sd3_lib_60.Extensions;
 using bifeldy_sd3_lib_60.Models;
 using bifeldy_sd3_lib_60.Repositories;
+using bifeldy_sd3_lib_60.Services;
 
 using bifeldy_sd3_mbz_60.Models;
 
@@ -16,17 +19,31 @@ namespace bifeldy_sd3_mbz_60.Abstractions {
 
         private readonly ENV _env;
 
+        private readonly IApplicationService _app;
+        private readonly IHttpService _http;
         private readonly IOraPg _orapg;
+        private readonly IConverterService _cs;
         private readonly IGeneralRepository _generalRepo;
+        private readonly IBaseService _baseService;
+
+        protected List<string> ExcludeJenisDc = null;
 
         public CBaseController(
             IOptions<ENV> env,
+            IApplicationService app,
+            IHttpService http,
             IOraPg orapg,
-            IGeneralRepository generalRepo
+            IConverterService cs,
+            IGeneralRepository generalRepo,
+            IBaseService baseService
         ) {
             _env = env.Value;
+            _app = app;
+            _http = http;
             _orapg = orapg;
+            _cs = cs;
             _generalRepo = generalRepo;
+            _baseService = baseService;
         }
 
         protected async Task<ObjectResult> CheckExcludeJenisDc(InputJsonDc fd, List<string> excludeJenisDc) {
@@ -62,12 +79,129 @@ namespace bifeldy_sd3_mbz_60.Abstractions {
                 return BadRequest(new ResponseJsonSingle<dynamic> {
                     info = $"🙄 403 - {GetType().Name} 😪",
                     result = new {
-                        message = $"Tidak dapat mengambil data pada DC {string.Join(", ", excludeJenisDc.ToArray())} karena masuk ke dalam daftar pengecualian"
+                        message = $"Tidak dapat mengambil data pada DC \"{string.Join(", ", excludeJenisDc.ToArray())}\" karena masuk ke dalam daftar pengecualian"
                     }
                 });
             }
 
             return null;
+        }
+
+        public virtual async Task<dynamic> HitDcAPI(InputJsonDc fd, string page, string row, string sort, string order) {
+            try {
+                if (fd == null || string.IsNullOrEmpty(fd?.kode_dc)) {
+                    return BadRequest(new ResponseJsonSingle<dynamic> {
+                        info = $"🙄 400 - {GetType().Name} 😪",
+                        result = new {
+                            message = "Format data tidak lengkap!"
+                        }
+                    });
+                }
+
+                string currentKodeDc = await _generalRepo.GetKodeDc();
+                if (currentKodeDc != "DCHO") {
+                    (decimal pages, decimal count, DataTable dt) = await _baseService.GetDataPaging(_env.IS_USING_POSTGRES, _orapg, fd, sort, order, page, row);
+
+                    return Ok(new ResponseJsonMulti<dynamic> {
+                        info = $"😅 201 - {GetType().Name} 🤣",
+                        results = dt.ToList<DC_PLANOGRAM_DISPLAY_V>(),
+                        pages = pages,
+                        count = count
+                    });
+                }
+                else {
+                    ObjectResult er = await CheckExcludeJenisDc(fd, ExcludeJenisDc);
+                    if (er != null) {
+                        return er;
+                    }
+
+                    string e = null;
+                    Uri u = null;
+                    await _generalRepo.GetDcApiPathAppFromHo(Request, fd.kode_dc, (err, res) => {
+                        e = err;
+                        u = res;
+                    });
+
+                    if (u == null) {
+                        return BadRequest(new ResponseJsonSingle<dynamic> {
+                            info = $"🙄 400 - {GetType().Name} 😪",
+                            result = new {
+                                message = e
+                            }
+                        });
+                    }
+
+                    using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(_cs.ObjectToJson(fd)))) {
+                        Request.Body = ms;
+                        return await _http.ForwardRequest(u.ToString(), Request, Response);
+                    }
+                }
+            }
+            catch (Exception ex) {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseJsonSingle<dynamic> {
+                    info = $"🙄 500 - {GetType().Name} 😪",
+                    result = new {
+                        message = _app.DebugMode ? ex.Message : "Terjadi kesalahan saat proses data"
+                    }
+                });
+            }
+        }
+
+        /* ** ALTERNATIVE ** */
+
+        public virtual async Task<IActionResult> DirectDbConnection(InputJsonDc fd, string page, string row, string sort, string order) {
+            try {
+                string currentKodeDc = await _generalRepo.GetKodeDc();
+                if (currentKodeDc != "DCHO") {
+                    return BadRequest(new ResponseJsonSingle<dynamic> {
+                        info = $"🙄 400 - {GetType().Name} (Mirror) 😪",
+                        result = new {
+                            message = "Endpoint ini hanya dapat diakses melalui HO"
+                        }
+                    });
+                }
+
+                if (fd == null || string.IsNullOrEmpty(fd?.kode_dc)) {
+                    return BadRequest(new ResponseJsonSingle<dynamic> {
+                        info = $"🙄 400 - {GetType().Name} (Mirror) 😪",
+                        result = new {
+                            message = "Format data tidak lengkap!"
+                        }
+                    });
+                }
+
+                ObjectResult er = await CheckExcludeJenisDc(fd, ExcludeJenisDc);
+                if (er != null) {
+                    return er;
+                }
+
+                (bool dbIsUsingPostgre, CDatabase dbOraPg, CDatabase dbMsSql) = await _generalRepo.OpenConnectionToDcFromHo(fd.kode_dc);
+                if (dbOraPg == null) {
+                    return BadRequest(new ResponseJsonSingle<dynamic> {
+                        info = $"🙄 400 - {GetType().Name} (Mirror) 😪",
+                        result = new {
+                            message = $"Kode gudang ({fd.kode_dc}) tidak tersedia!"
+                        }
+                    });
+                }
+
+                (decimal pages, decimal count, DataTable dt) = await _baseService.GetDataPaging(dbIsUsingPostgre, dbOraPg, fd, sort, order, page, row);
+
+                return Ok(new ResponseJsonMulti<dynamic> {
+                    info = $"😅 201 - {GetType().Name} (Mirror) 🤣",
+                    results = dt.ToList<DC_PLANOGRAM_DISPLAY_V>(),
+                    pages = pages,
+                    count = count
+                });
+            }
+            catch (Exception ex) {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseJsonSingle<dynamic> {
+                    info = $"🙄 500 - {GetType().Name} (Mirror) 😪",
+                    result = new {
+                        message = _app.DebugMode ? ex.Message : "Terjadi kesalahan saat proses data"
+                    }
+                });
+            }
         }
 
     }
--- bifeldy-sd3-mbz-60/Controllers/ProdMastController_.cs
+++ bifeldy-sd3-mbz-60/Controllers/ProdMastController_.cs
@@ -1,15 +1,10 @@
-﻿using System.Data;
-using System.Text;
-
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 
 using Swashbuckle.AspNetCore.Annotations;
 
-using bifeldy_sd3_lib_60.Abstractions;
 using bifeldy_sd3_lib_60.AttributeFilterDecorator;
 using bifeldy_sd3_lib_60.Databases;
-using bifeldy_sd3_lib_60.Extensions;
 using bifeldy_sd3_lib_60.Models;
 using bifeldy_sd3_lib_60.Repositories;
 using bifeldy_sd3_lib_60.Services;
@@ -24,17 +19,6 @@ namespace bifeldy_sd3_mbz_60.Controllers {
     [Route("prod-mast")]
     public sealed class ProdMastController : CBaseController {
 
-        private readonly ENV _env;
-
-        private readonly IApplicationService _app;
-        private readonly IHttpService _http;
-        private readonly IOraPg _orapg;
-        private readonly IConverterService _cs;
-        private readonly IGeneralRepository _generalRepo;
-        private readonly IProdMastService _prodMast;
-
-        private List<string> ExcludeJenisDc = new List<string> { "LPG", "IPLAZA" };
-
         public ProdMastController(
             IOptions<ENV> env,
             IApplicationService app,
@@ -43,83 +27,21 @@ namespace bifeldy_sd3_mbz_60.Controllers {
             IConverterService cs,
             IGeneralRepository generalRepo,
             IProdMastService prodMast
-        ) : base(env, orapg, generalRepo) {
-            _env = env.Value;
-            _app = app;
-            _http = http;
-            _orapg = orapg;
-            _cs = cs;
-            _generalRepo = generalRepo;
-            _prodMast = prodMast;
+        ) : base(env, app, http, orapg, cs, generalRepo, prodMast) {
+            ExcludeJenisDc = new List<string> { "LPG", "IPLAZA" };
         }
 
         [HttpPost]
         [MinRole(UserSessionRole.EXTERNAL_BOT)]
         [SwaggerOperation(Summary = "Ambil data Prod Mast dari / berdasarkan kode gudang tertentu (ex. G001)")]
-        public async Task<dynamic> HitDcAPI(
+        public override async Task<dynamic> HitDcAPI(
             [FromBody, SwaggerParameter("JSON body yang berisi kode gudang yang diminta", Required = true)] InputJsonDc fd,
             [FromQuery, SwaggerParameter("Angka nomor halaman yang diminta", Required = false)] string page = "1",
             [FromQuery, SwaggerParameter("Jumlah data yang di minta dalam satu halaman (max. 100)", Required = false)] string row = "10",
             [FromQuery, SwaggerParameter("Menggunakan key dari JSON yang akan di order", Required = false)] string sort = "plumd",
             [FromQuery, SwaggerParameter("ASC / DESC saat sorting", Required = false)] string order = "asc"
         ) {
-            try {
-                if (fd == null || string.IsNullOrEmpty(fd?.kode_dc)) {
-                    return BadRequest(new ResponseJsonSingle<dynamic> {
-                        info = $"🙄 400 - {GetType().Name} 😪",
-                        result = new {
-                            message = "Format data tidak lengkap!"
-                        }
-                    });
-                }
-
-                string currentKodeDc = await _generalRepo.GetKodeDc();
-                if (currentKodeDc != "DCHO") {
-                    (decimal pages, decimal count, DataTable dt) = await _prodMast.GetDataPaging(_env.IS_USING_POSTGRES, _orapg, fd, sort, order, page, row);
-
-                    return Ok(new ResponseJsonMulti<dynamic> {
-                        info = $"😅 201 - {GetType().Name} 🤣",
-                        results = dt.ToList<ProdMast>(),
-                        pages = pages,
-                        count = count
-                    });
-                }
-                else {
-                    ObjectResult er = await CheckExcludeJenisDc(fd, ExcludeJenisDc);
-                    if (er != null) {
-                        return er;
-                    }
-
-                    string e = null;
-                    Uri u = null;
-                    await _generalRepo.GetDcApiPathAppFromHo(Request, fd.kode_dc, (err, res) => {
-                        e = err;
-                        u = res;
-                    });
-
-                    if (u == null) {
-                        return BadRequest(new ResponseJsonSingle<dynamic> {
-                            info = $"🙄 400 - {GetType().Name} 😪",
-                            result = new {
-                                message = e
-                            }
-                        });
-                    }
-
-                    using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(_cs.ObjectToJson(fd)))) {
-                        Request.Body = ms;
-                        return await _http.ForwardRequest(u.ToString(), Request, Response);
-                    }
-                }
-            }
-            catch (Exception ex) {
-                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseJsonSingle<dynamic> {
-                    info = $"🙄 500 - {GetType().Name} 😪",
-                    result = new {
-                        message = _app.DebugMode ? ex.Message : "Terjadi kesalahan saat proses data"
-                    }
-                });
-            }
+            return await base.HitDcAPI(fd, page, row, sort, order);
         }
 
         /* ** */
@@ -127,65 +49,14 @@ namespace bifeldy_sd3_mbz_60.Controllers {
         [HttpPost("mirror")]
         [MinRole(UserSessionRole.EXTERNAL_BOT)]
         [SwaggerOperation(Summary = "Server cadangan (mirror) jika yang utama down")]
-        public async Task<IActionResult> DirectDcConnection(
+        public override async Task<IActionResult> DirectDbConnection(
             [FromBody] InputJsonDc fd,
             [FromQuery] string page = "1",
             [FromQuery] string row = "10",
             [FromQuery] string sort = "plumd",
             [FromQuery] string order = "asc"
         ) {
-            try {
-                string currentKodeDc = await _generalRepo.GetKodeDc();
-                if (currentKodeDc != "DCHO") {
-                    return BadRequest(new ResponseJsonSingle<dynamic> {
-                        info = $"🙄 400 - {GetType().Name} (Mirror) 😪",
-                        result = new {
-                            message = "Endpoint ini hanya dapat diakses melalui HO"
-                        }
-                    });
-                }
-
-                if (fd == null || string.IsNullOrEmpty(fd?.kode_dc)) {
-                    return BadRequest(new ResponseJsonSingle<dynamic> {
-                        info = $"🙄 400 - {GetType().Name} (Mirror) 😪",
-                        result = new {
-                            message = "Format data tidak lengkap!"
-                        }
-                    });
-                }
-
-                ObjectResult er = await CheckExcludeJenisDc(fd, ExcludeJenisDc);
-                if (er != null) {
-                    return er;
-                }
-
-                (bool dbIsUsingPostgre, CDatabase dbOraPg, CDatabase dbMsSql) = await _generalRepo.OpenConnectionToDcFromHo(fd.kode_dc);
-                if (dbOraPg == null) {
-                    return BadRequest(new ResponseJsonSingle<dynamic> {
-                        info = $"🙄 400 - {GetType().Name} (Mirror) 😪",
-                        result = new {
-                            message = $"Kode gudang ({fd.kode_dc}) tidak tersedia!"
-                        }
-                    });
-                }
-
-                (decimal pages, decimal count, DataTable dt) = await _prodMast.GetDataPaging(dbIsUsingPostgre, dbOraPg, fd, sort, order, page, row);
-
-                return Ok(new ResponseJsonMulti<dynamic> {
-                    info = $"😅 201 - {GetType().Name} (Mirror) 🤣",
-                    results = dt.ToList<ProdMast>(),
-                    pages = pages,
-                    count = count
-                });
-            }
-            catch (Exception ex) {
-                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseJsonSingle<dynamic> {
-                    info = $"🙄 500 - {GetType().Name} (Mirror) 😪",
-                    result = new {
-                        message = _app.DebugMode ? ex.Message : "Terjadi kesalahan saat proses data"
-                    }
-                });
-            }
+            return await base.DirectDbConnection(fd, page, row, sort, order);
         }
 
     }
